63abe889330bac0ecf0e32d4c8dc632b3b3298af
TarasMykytiuk <tarasmykytiukw@gmail.com>
2026-05-25 18:40:11 +0300

    cors_2

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 const quotes = [
     {
@@ -20,11 +23,8 @@ function pickRandomQuote() {
 }
 
 app.get("/", (req, res) => {
-    console.error("Received a request for a quote");
     const quote = pickRandomQuote();
     res.send(`"${quote.quote}" -${quote.author}`);
 });
 
-app.listen(port, () => {
-    console.error(`Quote server listening on port ${port}`);
-});
+app.listen(3000, () => console.log("Server running..."));
