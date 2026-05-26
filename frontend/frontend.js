4f1448c0276cb11735a741f30790da589292c9d5
TarasMykytiuk <tarasmykytiukw@gmail.com>
2026-05-27 02:47:46 +0300

    check frontend display

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,13 +1,15 @@
 async function getQuote() {
     const res = await fetch("https://dtjifu9aql4wip5jzpktv702.hosting.codeyourfuture.io/");
-    const quote = await res.json();
+    const quote = await res.text();
     return quote;
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
     try {
         const quote = await getQuote();
-        console.log(quote); // Use console.log instead of print()
+        console.log(quote);
+        display_quote = document.getElementById("display_quote");
+        display_quote.innerText = quote;
     } catch (error) {
         console.error("Failed to fetch the quote:", error);
     }
