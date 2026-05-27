cc416481cdfb7ff1c90878aa793e21ccc565dbe3
TarasMykytiuk <tarasmykytiukw@gmail.com>
2026-05-28 00:28:49 +0300

    post added

--- backend/index.js
+++ backend/index.js
@@ -22,9 +22,16 @@ function pickRandomQuote() {
     return quotes[index];
 }
 
-app.get("/", (req, res) => {
+app.get("/get_quote", (req, res) => {
     const quote = pickRandomQuote();
     res.send(`"${quote.quote}" -${quote.author}`);
+});
+
+app.post("/post_quote", (req, res) => {
+    const author = req.body.author;
+    const quote = req.body.quote;
+    quotes.push({ quote: quote, author: author });
+    return res.status(200).send({ message: "Success!" });
 });
 
 app.listen(3000, () => console.log("Server running..."));
--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,16 +1,47 @@
+const quoteDom = document.getElementById("display_quote");
+const authorInput = document.getElementById("author");
+const quoteInput = document.getElementById("new_quote");
+const submitBtn = document.getElementById("new_quote_submit");
+const changeBtn = document.getElementById("change_quote");
+
+document.addEventListener("DOMContentLoaded", async () => {
+    await changeDisplayedQuote();
+
+    changeBtn.addEventListener("click", async () => {
+        await changeDisplayedQuote();
+    })
+
+    submitBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        const author = authorInput.value;
+        const quote = quoteInput.value;
+        await postQuote(quote, author);
+    })
+});
+
+async function changeDisplayedQuote() {
+    try {
+        const quote = await getQuote();
+        quoteDom.innerText = quote;
+    } catch (error) {
+        console.error("Failed to fetch the quote:", error);
+    }
+}
+
 async function getQuote() {
-    const res = await fetch("https://dtjifu9aql4wip5jzpktv702.hosting.codeyourfuture.io/");
+    const res = await fetch("https://dtjifu9aql4wip5jzpktv702.hosting.codeyourfuture.io/get_quote");
     const quote = await res.text();
     return quote;
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
-    try {
-        const quote = await getQuote();
-        console.log(quote);
-        display_quote = document.getElementById("display_quote");
-        display_quote.innerText = quote;
-    } catch (error) {
-        console.error("Failed to fetch the quote:", error);
-    }
-});
+async function postQuote(quote, author) {
+    return fetch(
+        "https://dtjifu9aql4wip5jzpktv702.hosting.codeyourfuture.io/post_quote",
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ quote, author }),
+        }
+    );
+}
+
